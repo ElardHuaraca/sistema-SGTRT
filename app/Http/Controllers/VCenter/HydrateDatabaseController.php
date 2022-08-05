93b745cd78504fe8929178c51506da4f4d2da197
ElardHuaraca <elard.sd.9@gmail.com>
2022-08-05 09:16:33 -0500

    Implement generate project_generic if not exist and more logic

--- app/Http/Controllers/VCenter/HydrateDatabaseController.php
+++ app/Http/Controllers/VCenter/HydrateDatabaseController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\VCenter;
 
 use App\Helpers\DatabaseConnectionHelper;
 use App\Http\Controllers\Controller;
+use App\Models\Project;
+use App\Models\ResourceHistory;
+use App\Models\Server;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
@@ -11,21 +14,40 @@ class HydrateDatabaseController extends Controller
 {
     public static function hydrateDatabase($diskFile)
     {
+        /* register a generic project for servers that do not have information in the annotations */
+        $project_generic = Project::find('000000');
+        if (!$project_generic) {
+            Log::info('Creating generic project');
+            $project_generic = new Project();
+            $project_generic->idproject = '000000';
+            $project_generic->name = 'Generic Project';
+            $project_generic->save();
+        }
+
         foreach ($diskFile as $vcenter) {
 
             $connection = DatabaseConnectionHelper::setConnection($vcenter);
             $vms = $connection->select('SELECT
-                            vpx_vm.datacenter_id,
-                            vpx_vm.id,vpx_non_orm_vm_config_info."name",
-                            vpx_vm.guest_family,vpx_vm.mem_size_mb, vpx_vm.num_vcpu,
-                            vpx_vm.aggr_unshared_storage_space, vpx_vm.ip_address,
-                            vpx_non_orm_vm_config_info.hardware_memory as memory_ram_2,
-                            vpx_non_orm_vm_config_info.hardware_cores as cpu_cores_2,
-                            vpx_non_orm_vm_config_info.annotation
-                        FROM vpx_vm
-                        JOIN vpx_non_orm_vm_config_info on vpx_non_orm_vm_config_info.id=vpx_vm.id');
+                                            vpx_vm.id,vpx_non_orm_vm_config_info."name",
+                                            vpx_vm.guest_family, vpx_vm.num_vcpu,
+                                            vpx_vm.aggr_unshared_storage_space, vpx_vm.ip_address,
+                                            vpx_non_orm_vm_config_info.hardware_memory AS memory_ram,
+                                            vpx_non_orm_vm_config_info.annotation,vpx_datastore.name AS disk_name
+                                        FROM vpx_vm
+                                        JOIN vpx_non_orm_vm_config_info ON vpx_non_orm_vm_config_info.id = vpx_vm.id
+                                        LEFT JOIN vpx_vm_ds_space ON vpx_vm_ds_space.vm_id = vpx_vm.id
+                                        LEFT JOIN vpx_datastore ON vpx_datastore.id = vpx_vm_ds_space.ds_id');
             foreach ($vms as $vm) {
-                Log::info('Informacion : ' . $vm->datacenter_id);
+                $server = Server::where('name', $vm->name)->first();
+
+                if (!$server) {
+                    Log::info('Server not found: ' . $vm->name);
+                    $project = $vm->annotation;
+
+                    $explode_project = sizeof(explode('-', $project)) === 0 ? explode('_', $project) : explode('-', $project);
+                    if (count($explode_project) === 0) {
+                    }
+                }
             }
         }
     }
