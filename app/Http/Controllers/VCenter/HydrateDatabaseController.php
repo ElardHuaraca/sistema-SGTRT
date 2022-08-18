1bdcca8857e268a974828b2b4f7ebe66d7a20a24
ElardHuaraca <elard.sd.9@gmail.com>
2022-08-17 21:25:29 -0500

    Update rpa implement recolection data from disk information

--- app/Http/Controllers/VCenter/HydrateDatabaseController.php
+++ app/Http/Controllers/VCenter/HydrateDatabaseController.php
@@ -18,21 +18,16 @@ class HydrateDatabaseController extends Controller
         /* register a generic project for servers that do not have information in the annotations */
         $project_generic = Project::find('999999');
         if (!$project_generic) {
-            Log::info('Creating generic project');
-            $project_generic = new Project();
-            $project_generic->idproject = '999999';
-            $project_generic->name = 'GENERIC PROJECT';
-            $project_generic->save();
+            $project = HydrateDatabaseController::registerProject('999999', 'GENERIC PROJECT');
         }
 
         foreach ($diskFile as $vcenter) {
 
             $connection = DatabaseConnectionHelper::setConnection($vcenter);
             $vms = $connection->select('SELECT
-                                            vpx_vm.id,vpx_non_orm_vm_config_info."name",
-                                            vpx_vm.guest_family, vpx_vm.num_vcpu,
-                                            vpx_vm.aggr_unshared_storage_space, vpx_vm.ip_address,
-                                            vpx_non_orm_vm_config_info.hardware_memory AS memory_ram,
+                                            vpx_vm.id,vpx_non_orm_vm_config_info."name",vpx_vm.num_vcpu,
+                                            vpx_vm.aggr_unshared_storage_space, vpx_vm.aggr_uncommited_storage_space,
+                                            vpx_vm.power_state,vpx_non_orm_vm_config_info.hardware_memory AS memory_ram,
                                             vpx_non_orm_vm_config_info.annotation,vpx_datastore.name AS disk_name
                                         FROM vpx_vm
                                         JOIN vpx_non_orm_vm_config_info ON vpx_non_orm_vm_config_info.id = vpx_vm.id
@@ -45,17 +40,40 @@ class HydrateDatabaseController extends Controller
                     $project = $vm->annotation;
 
                     $explode_project = sizeof(explode('-', $project)) === 0 ? explode('_', $project) : explode('-', $project);
-                    if (count($explode_project) === 0) $explode_project = '999999';
+                    $explode_project[0] = trim($explode_project[0]);
+
+                    $service = null;
+                    $project = null;
+
+                    if (count($explode_project) !== 3 ||  !is_numeric($explode_project[0]) || strlen($explode_project[0]) !== 6) {
+                        $project = $project_generic;
+                        $service = 'BRONCE';
+                    } else {
+                        $explode_project[1] = trim($explode_project[1]);
+                        $explode_project[2] = strtoupper(trim($explode_project[2]));
+
+                        $project = Project::find($explode_project[0]);
+
+                        if (!$project) $project = HydrateDatabaseController::registerProject($explode_project[0], $explode_project[2]);
+
+                        $service = $explode_project[1];
+                    }
 
                     $server = new Server();
                     $server->name = strtoupper($vm->name);
-                    $server->active = strtoupper('999999' . $vm->name);
+                    $server->active = strtoupper($project->name . $vm->name);
                     $server->machine_name = strtoupper($vm->name);
                     $server->hostname = strtoupper($vm->name);
-                    $server->service = 'BRONCE';
-                    $server->idproject = '999999';
+                    $server->service = $service;
+                    $server->idproject = $project->idproject;
                     $server->save();
                 }
+
+                if ($vm->power_state === 0) {
+                    $vm->num_vcpu = 0;
+                    $vm->memory_ram = 0;
+                }
+
 
                 $resource_comsuption_ram = new ResourceHistory();
                 $resource_comsuption_ram->idserver = $server->idserver;
@@ -70,6 +88,21 @@ class HydrateDatabaseController extends Controller
                 $resource_comsuption_vcpu->amount = $vm->num_vcpu;
                 $resource_comsuption_vcpu->date = Carbon::now();
                 $resource_comsuption_vcpu->save();
+
+                /* Obtain type disk */
+                $disk_type = '';
+                if (str_contains($vm->disk_name, '_sas_') || str_contains($vm->disk_name, '_sat_') || str_contains($vm->disk_name, '_fls_')) {
+                    $disk_type = 'HDD';
+                } else if (str_contains($vm->disk_name, '_ssd_')) {
+                    $disk_type = 'SSD';
+                }
+
+                $resource_comsuption_disk = new ResourceHistory();
+                $resource_comsuption_disk->idserver = $server->idserver;
+                $resource_comsuption_disk->name = $disk_type;
+                $resource_comsuption_disk->amount = round(($vm->aggr_unshared_storage_space + $vm->aggr_uncommited_storage_space) / 1073741824);
+                $resource_comsuption_disk->date = Carbon::now();
+                $resource_comsuption_disk->save();
             }
         }
     }
@@ -78,22 +111,33 @@ class HydrateDatabaseController extends Controller
     {
         if (!Storage::disk('json')->exists('databases.json')) {
             /* if not exist file created */
-            Storage::disk('json')->put('databases.json', '
-            [
-                {
-                    "driver": "pgsql",
-                    "host": "ipexample",
-                    "port": "5432",
-                    "database": "vcenter",
-                    "username": "postgres",
-                    "password": "postgres",
-                    "schema": "public"
-                }
-            ]
-            ');
+            Storage::disk('json')->put(
+                'databases.json',
+                '[
+                    {
+                        "driver": "pgsql",
+                        "host": "ipexample",
+                        "port": "5432",
+                        "database": "vcenter",
+                        "username": "postgres",
+                        "password": "postgres",
+                        "schema": "public"
+                    }
+                ]'
+            );
         }
         /* get path from databases.json */
         $path = Storage::disk('json')->get('databases.json');
         return json_decode($path);
+    }
+
+    private static function registerProject($alp, $name)
+    {
+        $project = new Project();
+        $project->idproject = $alp;
+        $project->name = $name;
+        $project->save();
+
+        return $project;
     }
 }
